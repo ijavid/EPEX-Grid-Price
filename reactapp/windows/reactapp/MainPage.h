809c27ab84bd790886ba96eed47ed4ec482cb3be
David Pszota <david.pszota@wellapp.com>
2024-01-22 11:21:32 +0100

    add react native windows UWP

---
+++ reactapp/windows/reactapp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::reactapp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::reactapp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
